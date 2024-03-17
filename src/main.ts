4bbc6090c69f38f964dda6ab52bf10c2e9da672f
Mathenriques <math.marqui@gmail.com>
2024-03-16 21:34:05 -0300

    Add pipe na main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './users/users.module';
+import { UserModule } from './users/users.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(UserModule);
+
+  app.useGlobalPipes(new ValidationPipe())
+  
   await app.listen(3000);
 }
 bootstrap();
